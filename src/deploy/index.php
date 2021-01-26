8fa327524b672d000059a0b4858b60456da92704
Scott Harden <swharden@gmail.com>
2021-01-25 19:46:22 -0600

    functional deployment script

--- src/deploy/index.php
+++ src/deploy/index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "DEPLOY";
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    print("<pre>");
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+    print("</pre>");
+}
+
+echo "<h1>PULL</h1>";
+execPrint("git -C ../ pull https://github.com/ScottPlot/Website.git");
+
+echo "<h1>STATUS</h1>";
+execPrint("git status");
