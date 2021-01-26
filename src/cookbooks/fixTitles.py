e476996e7035c5ae40cad4c4eabbead0fda36c0e
Scott W Harden <swharden@gmail.com>
2021-01-25 23:58:13 -0500

    script to fix html titles

---
+++ src/cookbooks/fixTitles.py
@@ -0,0 +1,16 @@
+"""This script fixes old style <!-- titles --> to new style with frontmatter"""
+
+import os
+import glob
+
+for fname in glob.glob("*/index.md"):
+    with open(fname) as f:
+        lines = f.readlines()
+    version = os.path.dirname(fname)
+    frontmatter = "---\n" +\
+        "Title: ScottPlot %s Cookbook\n" % version +\
+        "---\n"
+    lines[0] = frontmatter + lines[0]
+    with open(fname, 'w') as f:
+        f.writelines(lines)
+    print("wrote", fname)
