989c8eb60c881fdbe96e3765e7ce52e5505002a8
Customer <u186-exgapiqqc7gg@usm25.siteground.biz>
2021-02-04 00:20:51 +0000

    upgrade to md2html version 3

--- src/cookbooks/rename.py
+++
@@ -1,9 +0,0 @@
-"""this script renames all readme.md files to index.md"""
-
-import os
-import glob
-
-for fname in glob.glob("*/readme.md"):
-    fname2 = os.path.dirname(fname)+"/index.md"
-    os.rename(fname, fname2)
-    print(fname2)
