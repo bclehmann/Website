fd7882c3d05cea499afca338619df70d27fe912d
Benjamin Lehmann <bclehmann@yahoo.ca>
2021-09-20 18:07:26 -0600

    Adds fsharp quickstart

---
+++ src/quickstart/src/quickstart-console-fsharp/Program.fs
@@ -0,0 +1,10 @@
+﻿open ScottPlot
+
+[<EntryPoint>]
+let main argv =
+    let dataX = [| 1.0 .. 5.0 |]
+    let dataY = [| for x in dataX -> x * x |]
+    let plt = Plot(400, 300);
+    plt.AddScatter(dataX, dataY);
+    plt.SaveFig("quickstart.png");
+    0
